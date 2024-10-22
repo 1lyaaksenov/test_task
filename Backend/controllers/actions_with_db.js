586cab27e5575d9899e10022e9b06a6cd7f7889f
1lyaaksenov <mane130320006@gmail.com>
2024-10-22 22:43:49 +0300

    Исправил функции бд

--- Backend/controllers/actions_with_db.js
+++ Backend/controllers/actions_with_db.js
@@ -1,14 +1,17 @@
 const pool = require('../config/init_db');
 
+// Для получения всех пользователей
 const getUsers = async (req, res) => {
   try {
     const result = await pool.query(`
       SELECT u.id_user, u.last_name, u.first_name, u.middle_name, u.birth_date, 
              u.passport, u.contact_info, u.address, u.salary, 
-             u.hire_date, d.department_name, s.status_name
+             u.hire_date, d.department_name, s.status_name, p.position_name
       FROM "user" u
       JOIN department d ON u.department_id = d.id_department
       JOIN status s ON u.status_id = s.id_status
+      JOIN user_position up ON u.id_user = up.user_id
+      JOIN position p ON up.position_id = p.id_position
     `);
     res.status(200).json(result.rows);
   } catch (err) {
@@ -16,50 +19,97 @@ const getUsers = async (req, res) => {
   }
 };
 
+// Для получения пользователей по отделу
 const getUsersByDepartment = async (req, res) => {
-  const { departmentId } = req.params;
+  const { departmentName } = req.params;
   try {
     const result = await pool.query(`
       SELECT u.id_user, u.last_name, u.first_name, u.middle_name, u.birth_date, 
              u.passport, u.contact_info, u.address, u.salary, 
-             u.hire_date, d.department_name, s.status_name
+             u.hire_date, d.department_name, s.status_name, p.position_name
       FROM "user" u
       JOIN department d ON u.department_id = d.id_department
       JOIN status s ON u.status_id = s.id_status
-      WHERE d.id_department = $1
-    `, [departmentId]);
+      JOIN user_position up ON u.id_user = up.user_id
+      JOIN position p ON up.position_id = p.id_position
+      WHERE d.department_name = $1
+    `, [departmentName]);
+    
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'Пользователи не найдены в указанном департаменте' });
+    }
+
     res.status(200).json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
-const addUser = async (req, res) => {
-  const { lastName, firstName, middleName, birthDate, passport, contactInfo, address, salary, hireDate, departmentId, statusId } = req.body;
+//Для получения пользователя по профессии 
+const getUsersByPosition = async (req, res) => {
+  const { positionName } = req.params;
   try {
     const result = await pool.query(`
-      INSERT INTO "user" (last_name, first_name, middle_name, birth_date, passport, 
-                          contact_info, address, salary, hire_date, department_id, 
-                          status_id)
-      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
-      RETURNING *
-    `, [lastName, firstName, middleName, birthDate, passport, contactInfo, address, salary, hireDate, departmentId, statusId]);
-    res.status(201).json(result.rows[0]);
+      SELECT u.id_user, u.last_name, u.first_name, u.middle_name, u.birth_date, 
+             u.passport, u.contact_info, u.address, u.salary, 
+             u.hire_date, d.department_name, s.status_name, p.position_name
+      FROM "user" u
+      JOIN department d ON u.department_id = d.id_department
+      JOIN status s ON u.status_id = s.id_status
+      JOIN user_position up ON u.id_user = up.user_id
+      JOIN position p ON up.position_id = p.id_position
+      WHERE p.position_name = $1
+    `, [positionName]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).json({ message: 'Пользователи с указанной должностью не найдены' });
+    }
+
+    res.status(200).json(result.rows);
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
+//Для добавления пользователя
+const addUser = async (req, res) => {
+  const { lastName, firstName, middleName, birthDate, passport, contactInfo, address, salary, hireDate, departmentId, statusId, positionId } = req.body;
+  try {
+    const resultUser = await pool.query(`
+      INSERT INTO "user" (last_name, first_name, middle_name, birth_date, passport, 
+                          contact_info, address, salary, hire_date, department_id, 
+                          status_id)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11)
+      RETURNING id_user
+    `, [lastName, firstName, middleName, birthDate, passport, contactInfo, address, salary, hireDate, departmentId, statusId]);
+
+    const userId = resultUser.rows[0].id_user;
+
+    // Подвязка к должности
+    await pool.query(`
+      INSERT INTO user_position (user_id, position_id)
+      VALUES ($1, $2)
+    `, [userId, positionId]);
+
+    res.status(201).json({ message: 'Пользователь добавлен', userId });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+
+// Для получения пользователя по полному ФИО
 const getUserByFullName = async (req, res) => {
   const { lastName, firstName, middleName } = req.params;
   try {
     const result = await pool.query(`
       SELECT u.id_user, u.last_name, u.first_name, u.middle_name, u.birth_date, 
              u.passport, u.contact_info, u.address, u.salary, 
-             u.hire_date, d.department_name, s.status_name
+             u.hire_date, d.department_name, s.status_name, p.position_name
       FROM "user" u
       JOIN department d ON u.department_id = d.id_department
       JOIN status s ON u.status_id = s.id_status
+      JOIN position p ON u.position_id = p.id_position
       WHERE u.last_name = $1 AND u.first_name = $2 AND 
             (u.middle_name = $3 OR u.middle_name IS NULL)
     `, [lastName, firstName, middleName]);
@@ -73,9 +123,10 @@ const getUserByFullName = async (req, res) => {
   }
 };
 
+//ДЛя обновления пользователя
 const updateUser = async (req, res) => {
   const { userId } = req.params;
-  const { lastName, firstName, middleName, birthDate, passport, contactInfo, address, salary, hireDate, departmentId, statusId } = req.body;
+  const { lastName, firstName, middleName, birthDate, passport, contactInfo, address, salary, hireDate, departmentId, statusId, positionId } = req.body;
   try {
     const result = await pool.query(`
       UPDATE "user"
@@ -91,19 +142,26 @@ const updateUser = async (req, res) => {
           department_id = $10,
           status_id = $11
       WHERE id_user = $12
-      RETURNING *
+      RETURNING id_user
     `, [lastName, firstName, middleName, birthDate, passport, contactInfo, address, salary, hireDate, departmentId, statusId, userId]);
-    
+
     if (result.rows.length === 0) {
-      return res.status(404).json({ message: 'Сотрудник не найден или уволен' });
+      return res.status(404).json({ message: 'Пользователь не найден' });
     }
-    
-    res.status(200).json(result.rows[0]);
+
+    await pool.query(`
+      UPDATE user_position
+      SET position_id = $1
+      WHERE user_id = $2
+    `, [positionId, userId]);
+
+    res.status(200).json({ message: 'Пользователь обновлен' });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
 };
 
+// Для увольнения пользователя(статус в таблице status)
 const dismissUser = async (req, res) => {
   const { userId } = req.params;
   try {
@@ -130,4 +188,5 @@ module.exports = {
   getUserByFullName,
   updateUser,
   dismissUser,
+  getUsersByPosition,
 };
--- Backend/routes/index.js
+++ Backend/routes/index.js
@@ -4,9 +4,10 @@ const userController = require('../controllers/actions_with_db');
 const router = express.Router();
 
 router.get('/users', userController.getUsers);
-router.get('/users/department/:departmentId', userController.getUsersByDepartment);
-router.post('/users', userController.addUser);
+router.get('/users/department/:departmentName', userController.getUsersByDepartment);
+router.get('/users/position/:positionName', userController.getUsersByPosition);
 router.get('/users/fullname/:lastName/:firstName/:middleName', userController.getUserByFullName);
+router.post('/users', userController.addUser);
 router.put('/users/:userId', userController.updateUser);
 router.delete('/users/:userId', userController.dismissUser);
 
